0b27777d8c4bae98e6a958ca7c720602220d448d
brunov0id <brunolabx@gmail.com>
2015-07-21 20:37:48 -0300

    add cardfragment

--- app/src/main/java/com/example/betweenbits/alarmdroid/MainActivity.java
+++ app/src/main/java/com/example/betweenbits/alarmdroid/MainActivity.java
@@ -6,25 +6,44 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.dexafree.materialList.cards.BasicButtonsCard;
+import com.dexafree.materialList.cards.OnButtonPressListener;
+import com.dexafree.materialList.cards.WelcomeCard;
+import com.dexafree.materialList.model.Card;
+import com.dexafree.materialList.view.MaterialListView;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
 import java.util.Calendar;
 
-
 public class MainActivity extends ActionBarActivity implements TimePickerDialog.OnTimeSetListener {
 
-    private TextView textView;
+    private FloatingActionButton floatingActionButton;
+    private MaterialListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
+        mListView = (MaterialListView) findViewById(R.id.material_listview);
+
+        welcome();
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.normal_plus);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                TimePickerDialog dpd = TimePickerDialog.newInstance(
+                        MainActivity.this,
+                        now.get(Calendar.HOUR),
+                        now.get(Calendar.MINUTE), true);
+                dpd.show(getFragmentManager(), "TimePickerDialog");
+            }
+        });
 
 //        Intent intent = new Intent("BroadcastReceiver_Alarm");
 //        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
@@ -33,23 +52,14 @@ public class MainActivity extends ActionBarActivity implements TimePickerDialog.
 //        calendar.setTimeInMillis(System.currentTimeMillis());
 //        calendar.add(Calendar.SECOND, 3);
 //
-//        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+//        AlarmManager alarmManager = (AlarmManager getSystemService(ALARM_SERVICE);
 //        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-    }
-
-    public void alarm(View view) {
-        Calendar now = Calendar.getInstance();
-        TimePickerDialog dpd = TimePickerDialog.newInstance(
-                MainActivity.this,
-                now.get(Calendar.YEAR),
-                now.get(Calendar.MONTH),true);
-        dpd.show(getFragmentManager(), "TimePickerDialog");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        cancel();
+//        cancel();
     }
 
     private void cancel() {
@@ -62,7 +72,29 @@ public class MainActivity extends ActionBarActivity implements TimePickerDialog.
 
     @Override
     public void onTimeSet(RadialPickerLayout radialPickerLayout, int hourOfDay, int minute) {
-        String time = "You picked the following time: "+hourOfDay+"h"+minute;
-        textView.setText(time);
+
+        BasicButtonsCard card = new BasicButtonsCard(getApplicationContext());
+        card.setTitle(hourOfDay + ":" + minute);
+
+        mListView.add(card);
+    }
+
+    private void welcome() {
+        WelcomeCard welcomeCard = new WelcomeCard(getApplicationContext());
+        welcomeCard.setTitle("Welcome to Alarm");
+        welcomeCard.setSubtitle("Add Your Alarms Here!");
+        welcomeCard.setDescription("Add Way Anytime.");
+        welcomeCard.setButtonText("OKAY!");
+
+        welcomeCard.setOnButtonPressedListener(new OnButtonPressListener() {
+            @Override
+            public void onButtonPressedListener(View view, Card card) {
+                mListView.remove(card);
+            }
+        });
+        welcomeCard.setDismissible(true);
+        welcomeCard.setBackgroundColorRes(R.color.background_material_dark);
+
+        mListView.add(welcomeCard);
     }
 }
