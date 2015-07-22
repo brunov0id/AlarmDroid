6bc6376a7847646b13e781065cd3d7a2717d8423
brunov0id <brunolabx@gmail.com>
2015-07-21 21:43:04 -0300

    implements cardadapter

--- app/src/main/java/com/example/betweenbits/alarmdroid/adapter/CardAdapter.java
+++ app/src/main/java/com/example/betweenbits/alarmdroid/adapter/CardAdapter.java
@@ -1,32 +1,67 @@
 package com.example.betweenbits.alarmdroid.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.example.betweenbits.alarmdroid.*;
+import com.example.betweenbits.alarmdroid.domain.Card;
+
+import java.util.List;
 
 /**
  * Created by brunov0id on 21/07/15.
  */
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.MyCarAdapter> {
 
+    private List<Card> listCard;
+    private LayoutInflater layoutInflater;
+
+    public CardAdapter(Context context, List<Card> listCard) {
+        this.listCard = listCard;
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
     @Override
     public MyCarAdapter onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = layoutInflater.inflate(R.layout.item, parent, false);
+
+        MyCarAdapter myCarAdapter = new MyCarAdapter(view);
+
+        return myCarAdapter;
     }
 
     @Override
     public void onBindViewHolder(MyCarAdapter holder, int position) {
-
+        holder.txtClock.setText(this.listCard.get(position).getClock());
+        holder.aSwitch.setChecked(this.listCard.get(position).getStatus());
+        holder.txtTitle.setText(this.listCard.get(position).getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.listCard.size();
     }
 
     public class MyCarAdapter extends RecyclerView.ViewHolder {
+
+        public TextView txtClock;
+        public Switch aSwitch;
+        public TextView txtTitle;
+        public ImageButton imgDelete;
+
         public MyCarAdapter(View itemView) {
             super(itemView);
+
+            txtClock  = (TextView) itemView.findViewById(R.id.txt_Clock);
+            aSwitch   = (Switch) itemView.findViewById(R.id.a_Switch);
+            txtTitle  = (TextView) itemView.findViewById(R.id.txt_Title);
+            imgDelete = (ImageButton) itemView.findViewById(R.id.img_Delete);
         }
     }
 }
--- app/src/main/java/com/example/betweenbits/alarmdroid/domain/Card.java
+++ app/src/main/java/com/example/betweenbits/alarmdroid/domain/Card.java
@@ -7,6 +7,39 @@ import java.util.Date;
  */
 public class Card {
     private long id;
-    private Date hora;
-    private String dias;
+    private String clock;
+    private String title;
+    private Boolean status;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getClock() {
+        return clock;
+    }
+
+    public void setClock(String clock) {
+        this.clock = clock;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
